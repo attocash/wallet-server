a33ca7f90b62904c770ef51645d3ae3210bb3dbb
rotilho <felipe@rotilho.com>
2025-11-15 22:04:02 +0100

    Bump to 5.4.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ ext["kotlin-coroutines.version"] = "1.9.0"
 ext["kotlin-serialization.version"] = "1.8.0"
 
 dependencies {
-    val commonsVersion = "d1eab26-SNAPSHOT"
+    val commonsVersion = "fc6a743-SNAPSHOT"
     val cucumberVersion = "7.23.0"
     val springdocVersion = "2.8.14"
 
